973fcb6b64198b54eba84032be0d5ec5e7471add
Jatin Gupta <201951073@iiitvadodara.ac.in>
2021-10-28 17:24:12 +0530

    admin api added

---
+++ backend/routes/routes.admin.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const adminController = require("../controllers/controller.admin");
+const route = express.Router();
+
+route.post("/login", adminController.login);
+route.post("/registerHospital", adminController.registerHospital);
+
+module.exports = route;
